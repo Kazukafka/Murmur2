a342a0f5d6776fd362589d5ad33ff9689844b7a9
Kazukafka <pasuwaado135@gmail.com>
2021-12-05 15:09:53 +0900

    admob settings

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -4,6 +4,8 @@ import { View, Text, Pressable, Alert } from 'react-native';
 import { generateKeyPair } from '../utils/crypt';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { User as UserModel } from '../src/models';
+import { AdMobBanner } from "expo-ads-admob";
+import { Platform } from "react-native";
 
 export const PRIVATE_KEY = "PRIVATE_KEY";
 
@@ -12,6 +14,19 @@ const Settings = () => {
     //await DataStore.clear(); <-まじで注意
     Auth.signOut();
   };
+
+  // テスト用のID
+  // 実機テスト時に誤ってタップしたりすると、広告の配信停止をされたりするため、テスト時はこちらを設定する
+  const testUnitID = Platform.select({
+    // https://developers.google.com/admob/ios/test-ads
+    ios: 'ca-app-pub-6500766760315589/8392796690',
+  });
+
+  // 実際に広告配信する際のID
+  // 広告ユニット（バナー）を作成した際に表示されたものを設定する
+  const adUnitID = Platform.select({
+    ios: 'ca-app-pub-6500766760315589/8392796690',
+  });
 
   const updateKeyPair = async () => {
     // generate private/public Key
@@ -42,6 +57,15 @@ const Settings = () => {
   return (
     <View>
       <Text>Settings</Text>
+
+      <View style={{ alignItems: "center", }}>
+        <AdMobBanner
+          bannerSize="smartBannerPortrait"
+          adUnitID={testUnitID}
+          servePersonalizedAds // パーソナライズされた広告の可否。App Tracking Transparencyの対応時に使用。
+        />
+      </View>
+
 
       <Pressable onPress={updateKeyPair} style={{ backgroundColor: 'blue', height: 50, margin: 10, borderRadius: 50, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{ color: 'white' }}>Update Keypair</Text>
